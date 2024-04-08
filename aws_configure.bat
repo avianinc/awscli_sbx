c9df53a0e56c24bc0b940f75ff4b945e4518bd16
John K. DeHart <64533653+johndehart@users.noreply.github.com>
2024-04-08 16:32:40 -0400

    added config + login scripts

---
+++ aws_configure.bat
@@ -0,0 +1,5 @@
+@echo off
+aws configure --profile 073860674057_VME_PowerUserAccess
+pause
+aws configure --profile 073860674057_VME_PowerUserAccess
+pause
--- aws_login.bat
+++ aws_login.bat
@@ -1,3 +1,5 @@
 @echo off
-aws sso login --profile 073860674057_VME_PowerUserAccess
+aws sso login --profile 073860674057_VME_PowerUserAccess --region us-gov-west-1
+pause
+aws sso login --profile 073860674096_VME_PowerUserAccess --region us-gov-west-1
 pause
